d8360605c9130034c535a7ac84de79ebc1a624b4
giriprasadabhimalla@gmail.com <giriprasadabhimalla@gmail.com>
2025-11-02 09:07:47 +0000

    dim_custaftcustorder

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -1,19 +1,6 @@
-{{ config(materialized="view") }}
-with customers as (
-    select
-    id as customer_id,
-    first_name,
-    last_name
-    from raw.jaffle_shop.customers
-),
-orders as  (
-    select 
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
-    from raw.jaffle_shop.orders
-),
+{{ config(materialized="table") }}
+with customers as (select * from {{ ref('stg_customers') }}),
+orders as (select * from {{ref('stg_order')}}),
 customer_orders as (
 select customer_id,
 min(order_date) as first_order_date,
