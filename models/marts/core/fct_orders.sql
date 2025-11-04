48446bfc3b1e4e55d5e767abc2a9f1011df7325a
giriprasadabhimalla@gmail.com <giriprasadabhimalla@gmail.com>
2025-11-04 14:46:12 +0000

    payments

---
+++ models/marts/core/fct_orders.sql
@@ -0,0 +1,15 @@
+with orders as (
+    select * from {{ref('stg_order')}}
+),
+payments as (
+    select * from {{ ref('stg_payment')}}
+),
+fct_orders as (
+    select order_id,
+    payment_id,
+    amount
+    from orders o
+    left join payments p on o.order_id = p.order_id
+)
+
+select * from fct_orders
